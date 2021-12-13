85d3124f8d4cca5a7202e64e64e0fd405d457fbb
Ugur Eren <ugureren@NETUK-H0YAMD6P.local>
2021-12-13 15:16:00 +0000

    changed code. To add new IP you must send JSON data via cURL post request

--- handlers/server-post-addIPtoPool.go
+++ handlers/server-post-addIPtoPool.go
@@ -1,6 +1,12 @@
 package handlers
 
 import (
+	"bytes"
+	"context"
+	"encoding/gob"
+	"encoding/json"
+	"fmt"
+	"ip-pool-manager/IP"
 	"net/http"
 
 	"github.com/go-redis/redis/v8"
@@ -8,5 +14,53 @@ import (
 
 func AddToIPtoPool(rdb *redis.Client) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-type", "application/json")
+
+		//	Creating a empty user post called "u"
+		var u IP.IPpost
+
+		//	Decodes response JSON into a userPostIP object and catches any errors
+		if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+			fmt.Println("ERR: ", err)
+			w.WriteHeader(http.StatusBadGateway)
+			w.Write([]byte("Cannot decode request"))
+			return
+		}
+
+		//	Checking if user IP value is correct lengh
+		if len(u.IPaddress) != 15 {
+			w.WriteHeader(http.StatusBadGateway)
+			resp := "IP is not correct length " + u.IPaddress
+			w.Write([]byte(resp))
+			return
+		}
+
+		ctx := context.Background()
+
+		fmt.Println(u)
+		fmt.Println(u.IPaddress)
+
+		encodedU := encodeIP(u)
+		//	Storing user key & value into db
+		rdb.Set(ctx, u.IPaddress, encodedU, 0)
+
+		userResponse := u.IPaddress + "IP has been added to DB"
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(userResponse))
 	}
+
 }
+
+//	Encodes IP into glob format
+func encodeIP(IP IP.IPpost) string {
+	// struct to Gob
+	bufEn := &bytes.Buffer{}
+	if err := gob.NewEncoder(bufEn).Encode(IP); err != nil {
+		panic(err)
+	}
+	BufEnString := bufEn.String()
+
+	return BufEnString
+}
+
+//  curl -X POST -H 'content-type: application/json' --data '{"ip":"a-222.2.222.222","detail":{"MACaddress":"89-43-5F-60-DC-76","leaseTime":"2021-12-13T11:11:52.106975Z","available":true}}' http://localhost:3000/addIPtoPool
